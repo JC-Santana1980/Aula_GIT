39686c1138786338b1bf01be318e9f646682b973
JC-Santana1980 <santana.juliocesar@gmail.com>
2026-05-06 21:44:04 -0300

    inicio

---
+++ projeto_senha/index.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+
+##Janela Principal Cliente
+cliente = Tk()
+cliente.title("Solicitar Senha")
+cliente.geometry ("300x250")
+cliente.configure(bg="lightgreen")
+
+##Variaveis Compartilhadas
+
+cliente.mainloop()
