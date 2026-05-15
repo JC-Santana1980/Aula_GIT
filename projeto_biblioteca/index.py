652b49b8ba2e8b82b6498c6d6cc48b826bbc22c8
JC-Santana1980 <santana.juliocesar@gmail.com>
2026-05-15 20:50:06 -0300

    Atualizacao final

--- projeto_biblioteca/index.py
+++ projeto_biblioteca/index.py
@@ -29,25 +29,25 @@ if len(st.session_state.livros) == 0:
      st.warning("Nenhum livro cadastrado.")
 else:
     for i, livro in enumerate(st.session_state.livros):
-         st.write(f"### {livro['nome']}")
-         st.write(f"Autor: {livro['autor']}")
+        st.write(f"### {livro['nome']}")
+        st.write(f"Autor: {livro['autor']}")
 
 #Status e Colunas.
-         if livro["emprestado"] == False:
+        if livro["emprestado"] == False:
              st.success("Disponível")
-         else:
+        else:
              st.error("Emprestado")
-col1, col2 = st.columns(2)
+        col1, col2 = st.columns(2)
 
-with col1:
-    if st.button(" Emprestar",key=f"emp{i}"):
-        st.session_state.livros[i]["emprestado"] = True
-        st.rerun()
+        with col1:
+            if st.button(" Emprestar",key=f"emp{i}"):
+                st.session_state.livros[i]["emprestado"] = True
+                st.rerun()
 
-    #Devolucao    
-    if st.button(" Devolver", key=f"dev{i}"):
-        st.session_state.livros[i]["emprestado"] = False
-        st.rerun()
-st.divider()    
+            #Devolucao    
+            if st.button(" Devolver", key=f"dev{i}"):
+                st.session_state.livros[i]["emprestado"] = False
+                st.rerun()
+        st.divider()    
  
 
